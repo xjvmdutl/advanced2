324bcb59b96345234e242c8905e2ea1ad7d4a036
Kimjunho <widn45@naver.com>
2022-04-10 17:23:53 +0900

    Proxy

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,23 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageDecorator implements Component{
+
+    private Component target;
+
+    public MessageDecorator(Component target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("MessageDecorator 실행");
+
+        String result = target.operation();
+        String decoResult = "*****" + result + "*****";
+        log.info("MessageDecorator 꾸미기 적용 전 = {}, 적용 후 = {}", result, decoResult);
+        return decoResult;
+    }
+}
---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,28 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    public void noProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    public void cacheProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
